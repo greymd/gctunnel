7a8069a6c579e6c287e72e66bd5e431b81fe49d3
greymd <yamadagrep@gmail.com>
2020-04-23 10:12:09 +0100

    Update naming rule

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/docopt/docopt-go"
@@ -17,47 +16,67 @@ Usage:
   gctunnel [Options]
   gctunnel auth
   gctunnel msgs <QUERY>
-  gctunnel msg <MSGID>
+  gctunnel msg <MSG_ID>
   gctunnel cals
-  gctunnel events --calendar-id <CALENDER_ID>
-  gctunnel create-event --calendar-id <CALENDER_ID> --summary <SUMMARY> --start <START_DATE_TIME> --end <END_DATE_TIME> [--description <DESCRIPTION>] [--timezone <TZ>]
+  gctunnel evts <CAL_ID> [--since=<datetime>]
+  gctunnel new-evt <CAL_ID> --summary=<text> --start=<datetime> --end=<datetime> [--description=<text>] [--timezone=<tz>]
 
 Options:
   -h, --help      Show help.
   -V, --version   Show version
 
-Default values:
-  DESCRIPTION     Empty
-  TZ              UTC
+Commands:
+  auth        Authentication Google API. Client ID/Secret are required. Visit Ref[1].
+  msgs        Search messages.
+  msg         Show body of single message.
+  cals        List calendars.
+  evts        List events, starting within last a week by default, on specified calendar.
+  new-evt     Create new event on the specified calendar.
+
+Arguments:
+  <QUERY>        Query for searching Gmail messages e.g "in:inbox". See Ref[2].
+  <MSG_ID>       Identifier of the message in Gmail. See Ref[3].
+  <CAL_ID>       Identifier of the calendar. See Ref[4].
+  <text>         Free format text.
+  <datetime>     A combined date-time value formatted according to RFC3339, e.g "2020-04-23T00:00:00Z"
+  <tz>           Time zone name with IANA Time Zone Database name, e.g "Asia/Tokyo". [default: UTC]
+
+Ref:
+  [1] https://console.developers.google.com
+  [2] https://support.google.com/mail/answer/7190
+  [3] https://developers.google.com/gmail/api/v1/reference/users/messages
+  [4] https://developers.google.com/calendar/v3/reference/calendars
 `
 
 func main() {
 	args, _ := docopt.ParseArgs(usage, nil, appVersion)
 	if args["auth"].(bool) {
+		// Delete token.json
 		auth.GetClient()
 		os.Exit(0)
 	}
-	fmt.Println(args)
+	// fmt.Println(args)
+	// os.Exit(0)
 	client := auth.GetClient()
 	if args["msgs"].(bool) {
 		messages.List(client, args["<QUERY>"].(string))
 	} else if args["msg"].(bool) {
-		messages.Get(client, args["<MSGID>"].(string))
-	} else if args["cals"] == true {
+		messages.Get(client, args["<MSG_ID>"].(string))
+	} else if args["cals"].(bool) {
 		events.ListCalendars(client)
-	} else if args["events"].(bool) {
-		events.ListEvents(client, args["<CALENDER_ID>"].(string))
-	} else if args["create-event"].(bool) {
-		fmt.Println(args)
-		description := args["<DESCRIPTION>"]
+	} else if args["evts"].(bool) {
+		events.ListEvents(client, args["<CAL_ID>"].(string))
+	} else if args["new-evt"].(bool) {
+		description := args["--description"]
 		if description == nil {
 			description = ""
 		}
-		timezone := args["<TZ>"]
+		timezone := args["--timezone"]
 		if timezone == nil {
 			timezone = "UTC"
 		}
-		events.CreateEvent(client, args["<CALENDER_ID>"].(string), args["<SUMMARY>"].(string), description.(string), args["<START_DATE_TIME>"].(string), args["<END_DATE_TIME>"].(string), timezone.(string))
+		os.Exit(0)
+		events.CreateEvent(client, args["<CAL_ID>"].(string), args["--summary"].(string), description.(string), args["--start"].(string), args["--end"].(string), timezone.(string))
 	}
 	os.Exit(0)
 }
