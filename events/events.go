8ebd8e111ecabf49e0f71ff3974c31833f05ba8b
greymd <yamadagrep@gmail.com>
2020-04-23 09:21:26 +0100

    works with struggling

---
+++ events/events.go
@@ -0,0 +1,70 @@
+package events
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"google.golang.org/api/calendar/v3"
+)
+
+// CreateEvent ... Create event
+func CreateEvent(client *http.Client, calendarID string, summary string, description string, startDateTime string, endDateTime string, timezone string) {
+	srv, err := calendar.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Calendar client: %v", err)
+	}
+
+	event := &calendar.Event{
+		Summary:     summary,
+		Location:    "",
+		Description: description,
+		Start: &calendar.EventDateTime{
+			DateTime: startDateTime,
+			TimeZone: timezone,
+		},
+		End: &calendar.EventDateTime{
+			DateTime: endDateTime,
+			TimeZone: timezone,
+		},
+	}
+
+	fixedEvent, err := srv.Events.Insert(calendarID, event).Do()
+	if err != nil {
+		log.Fatalf("Unable to create event. %v\n", err)
+	}
+	fmt.Printf("Event created: %s\n", fixedEvent.HtmlLink)
+}
+
+// ListCalendars ... list calenders
+func ListCalendars(client *http.Client) {
+	svc, err := calendar.New(client)
+	if err != nil {
+		log.Fatalf("Unable to create Calendar service: %v", err)
+	}
+	listRes, err := svc.CalendarList.List().Fields("items/id").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve list of calendars: %v", err)
+	}
+
+	for _, v := range listRes.Items {
+		cal, _ := svc.Calendars.Get(v.Id).Do()
+		fmt.Printf("Calendar ID: %v, %v\n", v.Id, cal.Summary)
+	}
+}
+
+// ListEvents ... List events
+func ListEvents(client *http.Client, calendarID string) {
+	svc, err := calendar.New(client)
+	if err != nil {
+		log.Fatalf("Unable to create Calendar service: %v", err)
+	}
+	// res, err := svc.Events.List(calendarID).Fields("items(start,end,summary)", "nextPageToken").UpdatedMin("2020-04-01T00:00:00Z").Do()
+	res, err := svc.Events.List(calendarID).Fields("items(start,end,summary)", "nextPageToken").UpdatedMin("2020-04-01T00:00:00Z").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve calendar events list: %v", err)
+	}
+	for _, v := range res.Items {
+		fmt.Printf("Calendar ID %q start: %v:, end: %v, summary:%q\n", calendarID, v.Start, v.End, v.Summary)
+	}
+}
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/docopt/docopt-go"
 	"github.com/greymd/gctunnel/auth"
+	"github.com/greymd/gctunnel/events"
 	"github.com/greymd/gctunnel/messages"
 )
 
@@ -13,14 +14,22 @@ var appVersion = `0.0.0`
 
 var usage = `
 Usage:
+  gctunnel [Options]
   gctunnel auth
-  gctunnel list-msgs <QUERY>
-  gctunnel get-msg <MSGID>
-  gctunnel register-event --subject <SUBJECT> --body <BODY> --time <TIME>
+  gctunnel msgs <QUERY>
+  gctunnel msg <MSGID>
+  gctunnel cals
+  gctunnel events --calendar-id <CALENDER_ID>
+  gctunnel create-event --calendar-id <CALENDER_ID> --summary <SUMMARY> --start <START_DATE_TIME> --end <END_DATE_TIME> [--description <DESCRIPTION>] [--timezone <TZ>]
 
 Options:
   -h, --help      Show help.
-  -V, --version   Show version`
+  -V, --version   Show version
+
+Default values:
+  DESCRIPTION     Empty
+  TZ              UTC
+`
 
 func main() {
 	args, _ := docopt.ParseArgs(usage, nil, appVersion)
@@ -28,13 +37,27 @@ func main() {
 		auth.GetClient()
 		os.Exit(0)
 	}
+	fmt.Println(args)
 	client := auth.GetClient()
-	if args["list-msgs"].(bool) {
+	if args["msgs"].(bool) {
 		messages.List(client, args["<QUERY>"].(string))
-	} else if args["get-msg"] == true {
+	} else if args["msg"].(bool) {
 		messages.Get(client, args["<MSGID>"].(string))
-	} else if args["register-event"] == true {
-		fmt.Println("this is register-event")
+	} else if args["cals"] == true {
+		events.ListCalendars(client)
+	} else if args["events"].(bool) {
+		events.ListEvents(client, args["<CALENDER_ID>"].(string))
+	} else if args["create-event"].(bool) {
+		fmt.Println(args)
+		description := args["<DESCRIPTION>"]
+		if description == nil {
+			description = ""
+		}
+		timezone := args["<TZ>"]
+		if timezone == nil {
+			timezone = "UTC"
+		}
+		events.CreateEvent(client, args["<CALENDER_ID>"].(string), args["<SUMMARY>"].(string), description.(string), args["<START_DATE_TIME>"].(string), args["<END_DATE_TIME>"].(string), timezone.(string))
 	}
 	os.Exit(0)
 }
